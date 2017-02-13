cbc05af8fcd293289834ecbfd6917b1f831757cb
William Cox <willubyu7@gmail.com>
2017-02-13 12:14:01 -0700

    changed nav

--- client/Nav.js
+++ client/Nav.js
@@ -7,11 +7,8 @@ export default class Nav extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD:client/Nav.js
         Hello
         {this.props.children}
-=======
->>>>>>> 87d11b2dfbab32ca6fe1bc31bfcd193631a9c5df:client/App.js
       </div>
     );
   }
