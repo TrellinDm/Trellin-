ab3585c8317b281281a1fc61d7f2d7f68a3817a6
Marcus Ogden <marcus@userlite.com>
2017-02-13 14:07:54 -0700

    save timeline

--- client/components/timeline/timeline.js
+++ client/components/timeline/timeline.js
@@ -1,3 +1,4 @@
+
 // import '../App.js';
 import React, { Component } from 'react';
 import CommentBox from './comments.js';
@@ -8,12 +9,6 @@ const profileImageThree = {backgroundImage: 'url(' + 'http://res.cloudinary.com/
 const profileImageOne = {backgroundImage: 'url(' + "http://kingofwallpapers.com/jack-black/jack-black-005.jpg" + ')'}
 const profileImageTwo = {backgroundImage: 'url(' + "http://www.gannett-cdn.com/-mm-/67c23b55d461f83e96855358c4bee23b00420bd6/c=334-0-5437-3836&r=x404&c=534x401/local/-/media/USATODAY/USATODAY/2014/04/03//1396538883000-James-Franco.jpg" + ')'}
 
-constructor() {
-  super();
-  this.state = {
-
-  }
-}
 
 
 class Timeline extends React.Component {
