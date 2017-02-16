3decd02a7eb2b1845143413d547b0915cc0c7f93
William Cox <willubyu7@gmail.com>
2017-02-16 12:13:20 -0700

    added connections view

---
+++ client/components/connections/Connections.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {Link} from 'react-router';
+import {setConnections} from '../../reducers/connectionsReducer';
+import axios from 'axios';
+import './connections.scss';
+
+class Connections extends Component {
+
+  componentDidMount() {
+    axios.post('/getConnections', {
+      id: 1
+    }).then(res => {
+      this.props.setConnections(res.data);
+    });
+  }
+
+  render() {
+    var test = this.props.connections.map((conn, i) => {
+      console.log(conn);
+      var name = conn.first + " " + conn.last;
+      return (
+        <div key={i} className="conn section-shadow">
+          <img src={conn.s3} />
+          <div className="conn-info">
+            <p>{name}</p>
+            <p>{conn.headline}</p>
+          </div>
+          <Link to="/profile"><button className="button-connect">View Profile</button></Link>
+        </div>
+      )
+    });
+    return (
+      <div className="connections">{test}</div>
+    )
+  }
+}
+
+const mapDispatchToProps = {
+  setConnections: setConnections
+}
+
+function mapStateToProps(state) {
+  return {
+    connections: state.connections.connections
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Connections);
--- client/rootReducer.js
+++ client/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import search from './reducers/searchReducer';
 import list from './reducers/listReducer';
+import connections from './reducers/connectionsReducer';
 
 export default combineReducers({
   search,
-  list
+  list,
+  connections
 });
