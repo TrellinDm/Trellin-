e9c9cbd72df7849e20a758e0e7d3a92f1f124ba2
William Cox <willubyu7@gmail.com>
2017-02-17 13:05:10 -0700

    connected profile to db

--- client/rootReducer.js
+++ client/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import search from './reducers/searchReducer';
 import list from './reducers/listReducer';
 import connections from './reducers/connectionsReducer';
+import profile from './reducers/profileReducer';
 
 export default combineReducers({
   search,
   list,
-  connections
+  connections,
+  profile
 });
